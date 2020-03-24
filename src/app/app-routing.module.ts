949213c8938e2968bc964ba67f141f4b1ac4e4e5
carlosalurban <carlosalvarezurbano@gmail.com>
2020-03-24 10:36:38 +0100

    fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 const routes: Routes = [
   { path: 'home', component: ContainerComponent },
   { path: 'profile/:userId', component: ProfileComponent },
-  { path: '', component: ContainerComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 
 ];
